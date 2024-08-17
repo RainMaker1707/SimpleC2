1d251de8a710fb611b7b0c9b76a71069373c338d
RainMaker1707 <hadrien.allegaert@student.uclouvain.be>
2024-08-17 15:02:10 +0200

    update client and server two work on separate devices

--- client/client.py
+++ client/client.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from http.server import BaseHTTPRequestHandler
 from argparse import ArgumentParser
 from http.server import HTTPServer
 from time import sleep, time
@@ -8,27 +9,46 @@ import requests
 import socket
 import random
 
-from modules.simple_handler import SimpleHandler
+class SimpleHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"<html><body><h1>Success</h1></body></html>")
+        if self.path[:10] == "/pictures/" and self.path[-4:] == ".png":
+            print("Killed")
+            stop_event.set()
+
+
+    def do_POST(self):
+        pass
+
+    def do_PUT(self):
+        pass
+
+    def do_DELETE(self):
+        pass
     
 
 def heartbeat(stop_event, server_ip):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    t = 30 + random.randint(1, 31)
+    t = (30 + random.randint(1, 31)) * 1
     last = 0
     while not stop_event.is_set():
-        if time() < last + t:
+        current = time()
+        if current >= last + t or last==0: # simulate sleep but letting the stop_event kill the loop, using sleep doesn't let it happens
             sock.sendto(b'Alive', (server_ip, 5000))
-            print("Heartbeat sent.") 
-            sleep(t)
-            t = 30 + random.randint(1, 31)
-            last = time()
-
+            t = (30 + random.randint(1, 31)) * 1
+            print(f'Heartbeat sent. Next heartbeat in {t}s.') 
+            last = current
+        sleep(.5)
 
 def command_loop(stop_event):
     while not stop_event.is_set():
         match input("$: "):
             case "exit": stop_event.set()
-            case _: print("Command not recognized")
+            case _: pass
 
 
 def run_server(stop_event, ip="0.0.0.0", port=8080, server_class=HTTPServer, handler_class=SimpleHandler):
@@ -39,14 +59,14 @@ def run_server(stop_event, ip="0.0.0.0", port=8080, server_class=HTTPServer, han
     while not stop_event.is_set():
         httpd.handle_request()  # Handle a single request at a time with a timeout
     httpd.server_close()
-    print("HTTP server stopped.")
+    print("HTTP server stopped.\nType ENTER to quit.")
 
 
 if __name__ == "__main__":
     parser = ArgumentParser(prog="SimpleClient")
     parser.add_argument("ip", type=str)
     args = parser.parse_args()
-    print(args.ip)
+    print("Server at: ", args.ip)
     stop_event = Event()
     t1 = Thread(target=heartbeat, args=(stop_event, args.ip, ))
     t2 = Thread(target=command_loop, args=(stop_event, ))
--- server/server.py
+++ server/server.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
-from http.server import HTTPServer
+from http.server import HTTPServer, BaseHTTPRequestHandler
 from threading import Thread, Event
+from time import time, sleep
 
 import sys
 import requests
 import random
 import socket
 
-from modules.simple_handler import SimpleHandler
 from dictionnaries.file_name import files
 from dictionnaries.task_name import tasks
 
+client_list = dict()
+timeout_max = 60
+connected = False
+PORT = 8080
 
 def help():
     print("Here is the help. You'll don't have real help as it is a fictional framework")
@@ -25,8 +29,7 @@ def send(ip, port, content="", task=True):
 
 
 
-def build_uri_task(ext='.js'):
-    uri_start = '/scripts/'
+def build_uri_task(uri_start = '/scripts/', ext='.js'):
     return uri_start + random.choice(tasks) + ext
 
 
@@ -34,7 +37,7 @@ def build_uri_file(uri_start='/pictures/', ext='.png'):
     return uri_start + random.choice(files) + ext
 
 
-def run_server(stop_event, ip="0.0.0.0", port=8000, server_class=HTTPServer, handler_class=SimpleHandler):
+def run_server(stop_event, ip="0.0.0.0", port=8000, server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
     server_address = (ip, port)
     httpd = server_class(server_address, handler_class)
     # Set a timeout for handle_request to periodically check stop_event
@@ -47,14 +50,47 @@ def run_server(stop_event, ip="0.0.0.0", port=8000, server_class=HTTPServer, han
 
 def command_loop(stop_event):
     while not stop_event.is_set():
-        user_in = input("$: ")
-        match user_in:
-            case "help": help()
-            case "screen": pass # will use order model
-            case "ls": send("127.0.0.1", 8080) # will use tasking model
-            case "kill": pass # will kill the distant process
-            case "exit": stop_event.set()
-            case _: print("Command not recognized")
+        user_in = input("$: ").split()
+        try:
+            match user_in[0]:
+                case "help": help()
+                case "kill": send(connected, PORT, task=False)
+                case "ls": send(connected, PORT)
+                case "clients": 
+                    show_clients()
+                    print("Type `use <ip>` to connect to a client.")
+                case "use": 
+                    if len(user_in) != 2: 
+                        print("Usage: `use <ip>`") 
+                        return
+                    use_cmd(user_in[1])
+                case "exit": stop_event.set()
+                case _: print("Command not recognized.")
+        except IndexError:
+            pass
+
+
+def use_cmd(ip):
+    global connected
+    if connected:
+        print("Already connected.")
+    else:
+        if ip not in client_list.keys():
+            print("Unrecognized client.")
+            return
+        connected = ip
+        print("Now using: ", connected)
+        URL=build_uri_file(uri_start="/images/", ext=".png")
+        requests.get(url=f'http://{connected}:{PORT}{URL}', params={})
+
+
+def show_clients():
+    current = time()
+    if client_list.keys():
+        for key in client_list.keys():
+            print(f'IP: {key}, last time heard: {current - client_list[key]: .2f}s ago.')
+    else:
+        print("No clients connected.")
 
 
 def receive_heartbeat(stop_event):
@@ -62,7 +98,28 @@ def receive_heartbeat(stop_event):
     sock.bind(("0.0.0.0", 5000))
     while not stop_event.is_set():
         data, addr = sock.recvfrom(512)
-        print(f'received {data} from {addr}')
+        if not addr[0] in client_list.keys():
+            print("New client connected: ", addr[0])
+        client_list[addr[0]] = time()
+    print("Hearbeat receiver stopped")
+
+
+def check_neighbor(stop_event):
+    to = 20
+    last = 0
+    while not stop_event.is_set():
+        current = time()
+        if current > last + to or last == 0:
+            last = current
+            to_del = False
+            for key in client_list.keys():
+                if client_list[key] + timeout_max < current:
+                    to_del = key
+                    break
+            if to_del:
+                del client_list[to_del]
+        sleep(0.1)
+    print("Check neighbor stopped")
 
 
 
@@ -76,7 +133,7 @@ if __name__ == "__main__":
     print("HTTP server listening.")
     t2 = Thread(target=command_loop, args=(stop_event, ))
     t3 = Thread(target=receive_heartbeat, args=(stop_event, ))
+    t4 = Thread(target=check_neighbor, args=(stop_event, ))
     
-    t1.start(), t2.start(), t3.start()
-
-    t1.join(), t2.join(), t3.join()
+    t1.start(), t2.start(), t3.start(), t4.start()
+    t1.join(), t2.join(), t3.join(), t4.join()
